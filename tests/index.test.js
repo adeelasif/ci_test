9baa7dbe3b47b4fd8498aa04def766eb499e89df
Adeel Asif <adeel.asif.ali@hotmail.com>
2020-02-19 10:16:00 +0500

    Initial Commit

---
+++ tests/index.test.js
@@ -0,0 +1,15 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Authentication', () => {
+  describe('Login', () => {
+    it('should fail', async () => {
+
+      expect(false).to.be.true;
+    });
+    it('should pass', async () => {
+
+      expect(true).to.be.true;
+    });
+  });
+});
